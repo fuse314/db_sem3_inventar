3affdebafe434821f6b8a083eac2aa562bb4442b
anonuem <eliasschwarz@Elias-Schwarzs-MacBook-Pro.local>
2015-03-10 18:08:15 +0100

    Tabelle Person erstellt, Reihenfolge angepasst

--- structure/05_create_customer.sql
+++ structure/05_create_customer.sql
@@ -1,8 +1,7 @@
 -- Tabelle Customer wird erstellt mit Verknpüfung zur Adresse. (Rechnungsadresse und normale Adresse)
 CREATE TABLE Customer (
   Id_Customer int auto_increment
-, FirstName varchar(42)
-, SurName varchar(42)
+, Name varchar(42)
 , Id_InvoiceAddress int
 , Id_Address int NOT NULL
 , PRIMARY KEY (Id_Customer)
---
+++ structure/08_create_person.sql
@@ -0,0 +1,10 @@
+
+-- Tabelle Customer wird erstellt mit Verknpüfung zur Adresse. (Rechnungsadresse und normale Adresse)
+CREATE TABLE Person (
+  Id_Person int auto_increment
+, FirstName varchar(42)
+, SurName varchar(42)
+, Id_InvoiceAddress int
+, Id_Address int NOT NULL
+, PRIMARY KEY (Id_Customer)
+, FOREIGN KEY (Id_Address) REFERENCES Address(Id_Person))
