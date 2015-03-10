2113481bae229f88b1749e349eabdec96b5d29cb
Gottfried Mayer <github@gma.name>
2015-03-10 19:57:31 +0100

    relnetworkinterface

--- structure/23_relnetworkinterface.sql
+++ structure/23_relnetworkinterface.sql
@@ -1,9 +1,9 @@
-CREATE TABLE NetworkInterfaceConnection (
-  ID_NetworkInterfaceConnection INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE RelNetworkInterface (
+  ID_RelNetworkInterface INT NOT NULL AUTO_INCREMENT,
   ID_NetworkInterfaceA INT NOT NULL,
   ID_NetworkInterfaceB INT NOT NULL,
   
-  PRIMARY KEY (ID_NetworkInterfaceConnection),
+  PRIMARY KEY (ID_RelNetworkInterface),
   FOREIGN KEY (ID_NetworkInterfaceA) REFERENCES NetworkInterface(ID_NetworkInterface),
   FOREIGN KEY (ID_NetworkInterfaceB) REFERENCES NetworkInterface(ID_NetworkInterface)
   );
