ff00e7d21716e1ad2e958ce53e6edd57864f5c8a
Gottfried Mayer <github@gma.name>
2015-03-10 19:54:52 +0100

    devicecategory

---
+++ example_data/25_devicecategory.sql
@@ -0,0 +1,5 @@
+INSERT INTO DeviceCategory (ID_DeviceCategory, Description) VALUES (1,'Switch');
+INSERT INTO DeviceCategory (ID_DeviceCategory, Description) VALUES (2,'Workstation');
+INSERT INTO DeviceCategory (ID_DeviceCategory, Description) VALUES (3,'Virtual Machine');
+INSERT INTO DeviceCategory (ID_DeviceCategory, Description) VALUES (4,'Printer');
+
---
+++ structure/25_devicecategory.sql
@@ -0,0 +1,6 @@
+CREATE TABLE DeviceCategory (
+  ID_DeviceCategory INT NOT NULL AUTO_INCREMENT,
+  Description VARCHAR(100),
+  
+  PRIMARY KEY (ID_DeviceCategory)
+  );
