b6c7b4779e89cbf19705276f4c926b43da8c0e3d
Gottfried Mayer <github@gma.name>
2015-03-17 13:49:54 +0100

    example data device category

---
+++ example_data/insert devicecategory.sql
@@ -0,0 +1,8 @@
+CREATE TABLE DeviceCategory(ID_DeviceCategory,Description) VALUES (1,'Switch');
+CREATE TABLE DeviceCategory(ID_DeviceCategory,Description) VALUES (2,'Router');
+CREATE TABLE DeviceCategory(ID_DeviceCategory,Description) VALUES (3,'WLAN Accesspoint');
+CREATE TABLE DeviceCategory(ID_DeviceCategory,Description) VALUES (4,'Printer');
+CREATE TABLE DeviceCategory(ID_DeviceCategory,Description) VALUES (5,'Workstation');
+CREATE TABLE DeviceCategory(ID_DeviceCategory,Description) VALUES (6,'Server');
+
+
